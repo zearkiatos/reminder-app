e9a6defc41118fcdfc4e0093b0c87dd219da6c29
Pedro Capriles M <p0c04cw@M-C02Z9029LVCG.local>
2022-02-11 08:49:06 -0300

    [Work in Progress] Initialization project

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import data from './mocks/data.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        renderItem={}
+      >
+
+      </FlatList>
     </View>
   );
 }
